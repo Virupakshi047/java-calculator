017ce1ca6452d93aed54b13d019c983601f7d5d0
Virupakshi <virupakshivijji@gmail.com>
2025-02-10 12:25:56 +0530

    declared functions

---
+++ Application.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Application {
+        public boolean isValidExpression(String express){
+
+            return true;
+        }
+        public int calculate(String expression){
+
+            return  0;
+        }
+    public static void main(String[] args) {
+
+    }
+}
